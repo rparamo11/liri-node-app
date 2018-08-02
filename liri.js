49cca412c3343acfda841d88fc178471bfc8d48e
rparamo <rparamo11@gmail.com>
2018-08-01 23:40:44 -0500

    added twitter code

--- liri.js
+++ liri.js
@@ -1,2 +1,39 @@
-js
 require("dotenv").config();
+
+var keys = require("./keys");
+console.log(keys);
+
+var Twitter = require("twitter");
+
+var getMyTweets = function (){
+    
+    var twitterClient = new Twitter(keys.twitter)
+    var params = {
+        screen_name: "cnn"
+    }
+    twitterClient.get("statuses/user_timeline", params, function(error, tweets,response){
+        if(!error){
+            var newInfo = tweets.map(tweet => {
+                return {
+                    date: tweet.created_at,
+                    tweet: tweet.text
+                }
+            })
+            newInfo.forEach(info => {
+                console.log('=======================')
+                console.log('Date:', info.date)
+                console.log('Tweet:', info.tweet)
+                console.log('======================')
+            })
+        }
+        if(error){
+            console.log('ERRROR!==>', error)
+        }
+
+    })
+};
+
+getMyTweets()
+
+
+
